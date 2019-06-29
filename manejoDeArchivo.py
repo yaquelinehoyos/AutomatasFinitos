08c94f60acf00db7f88e3b1e61ae46776b6357c8
Yaqueline Hoyos <yaqueline150@gmail.com>
2019-06-28 20:18:45 -0500

    Método: comprobarTransicionesAEstadosCorrectos()

--- manejoDeArchivo.py
+++ manejoDeArchivo.py
@@ -8,28 +8,45 @@ class ManejoDeArchivo:
         tablaDeTransiciones = []
         file1 = open( nombreDelArchivo, "r" )
         lista = file1.readlines()
-
         for val in range( len( lista ) ):
             tablaDeTransiciones.append( lista[val].rstrip( '\n' ).split( ',' ) )
-
         file1.close()
         self.comprobarFormatoDelArchivo( tablaDeTransiciones )
+        listaDeEstados = self.comprobarListaDeEstados( tablaDeTransiciones )
+        self.comprobarTransicionesAEstadosCorrectos( tablaDeTransiciones, listaDeEstados )
         return tablaDeTransiciones
 
 
     def comprobarFormatoDelArchivo( self, lista ):
         if ( len( lista[0] ) != ( len( lista[1] ) - 2 ) ):
-            print( "Verifique el archivo, el número de entradas no conincide con las transiciones" )
+            print( "El número de entradas no conincide con las transiciones" )
+            print( "verifique el archivo en la fila: 1" )
             exit()
-
         for val in range ( 2, len( lista ) ):
             anterior = val - 1
             if ( len( lista[val] ) != len( lista[anterior] ) ):
-                print( "Verifique el archivo, existe un estado con más o menos transiciones que los demás" )
+                print( "Existe un estado con más o menos transiciones que los demás" )
+                print( "Verifique el archivo en la fila: ", val )
                 exit()
-        
         for val in range( 1, len( lista ) ):
             if ( (lista[val][-1] != '0') and (lista[val][-1] != '1') ):
-                print( "Verifique el archivo, algún estado de aceptación es incorrecto" )
+                print( "Algún estado de aceptación es incorrecto" )
+                print( "Verifique el archivo en la fila: ", val )
                 exit()
         
+
+    def comprobarListaDeEstados( self, lista ):
+        listaEstados = []
+        for val in range( 1, len( lista ) ):
+            listaEstados.append( lista[val][0] )
+        return listaEstados
+
+
+    def comprobarTransicionesAEstadosCorrectos( self, lista, listaEstados ):
+        for x in range( 1, len(lista) ):
+            for y in range( 1, (len(lista[x]) - 1) ):
+                if( listaEstados.count( lista[x][y] ) == 0 ):
+                    print( "Alguna de las transiciones está incorrecta" )
+                    print( "Verifique el archivo en la fila: ", x )
+                    exit()
+                
