efa83c99b17a0c0b33399fa38c9fd4ccc0679b75
Yaqueline Hoyos <yaqueline150@gmail.com>
2019-06-29 17:33:25 -0500

    Verificar formato de nuevos estados

--- clasePrincipal.py
+++ clasePrincipal.py
@@ -4,22 +4,27 @@ import manejoDeArchivo as ma
 archivo = ma.ManejoDeArchivo()
 nombreDelArchivo = "archivo.txt"
 
-booleanoAuxiliar = True
-bandera = False
+booleanoAuxiliar = True     # Esta variable nos será útil si el usuario desea ingresar varias filas
+bandera = False             # Esta variable nos indica si se ingresó una nueva fila al archivo
+tablaDeTransiciones = archivo.obtenerTablaDeTransicionesDesdeArchivo( nombreDelArchivo, bandera )
+print( "Esta es la tabla ingresada: ", tablaDeTransiciones )
+
 while( booleanoAuxiliar ):
-    tablaDeTransiciones = archivo.obtenerTablaDeTransicionesDesdeArchivo( nombreDelArchivo, bandera ) 
-    automataFinito = af.AutomataFinito( tablaDeTransiciones )
-
-    print( "\nTabla de transiciones: \n", automataFinito.obtenerTablaDeTransiciones() )
-    print( "\nNumero de estados: ", automataFinito.obtenerNumeroDeEstados() )
-    print( "\nEntradas: ", automataFinito.obtenerEntradas() )
-
     pregunta = input( "\n¿Desea ingresar una nueva fila? y/n: \n" )
     if( pregunta == 'y' ):
         bandera = True
         file1 = open( nombreDelArchivo, "a" )
         nuevaFila = input( "\nIngrese la nueva fila\n" )
-        file1.write( "\n" + nuevaFila )
+        file1.write( "\n" + nuevaFila )         # Añadimos la nueva fila al archvio
         file1.close()
     elif( pregunta == 'n' ):
+        bandera = False
         booleanoAuxiliar = False
+    # En la siguiente línea se verifica de nuevo el archivo en caso de que se haya agregado una nueva fila al archivo
+    tablaDeTransiciones = archivo.obtenerTablaDeTransicionesDesdeArchivo( nombreDelArchivo, bandera )
+
+    
+automataFinito = af.AutomataFinito( tablaDeTransiciones )
+print( "\nTabla de transiciones: \n", automataFinito.obtenerTablaDeTransiciones() )
+print( "\nNumero de estados: ", automataFinito.obtenerNumeroDeEstados() )
+print( "\nEntradas: ", automataFinito.obtenerEntradas() )
--- manejoDeArchivo.py
+++ manejoDeArchivo.py
@@ -1,51 +1,76 @@
 class ManejoDeArchivo:
 
     nombreArchivo = ''
+    rompeCiclos = False     # Esta variable nos sirve como bandera para eliminar sólo la fila incorrecta
 
+    # Constructor
     def __init__( self ):
         pass
 
 
+    # El siguiente método se llama desde la clase principal para verificar que el archivo tenga el formato correcto
     def obtenerTablaDeTransicionesDesdeArchivo( self, nombreDelArchivo, lineaNueva ):
         self.nombreArchivo = nombreDelArchivo
+        self.rompeCiclos = False
         tablaDeTransiciones = []
         file1 = open( self.nombreArchivo, "r" )
-        lista = file1.readlines()
-        for val in range( len( lista ) ):
-            tablaDeTransiciones.append( lista[val].rstrip( '\n' ).split( ',' ) )
+        lista = file1.readlines()   # El método readlines() nos devuelve una lista
         file1.close()
+        for val in range( len( lista ) ):
+            tablaDeTransiciones.append( lista[val].rstrip( '\n' ).split( ',' ) )    # Quitamos los saltos de línea y las comas
         self.comprobarFormatoDelArchivo( tablaDeTransiciones, lineaNueva )
         listaDeEstados = self.buscarListaDeEstados( tablaDeTransiciones )
         self.comprobarTransicionesAEstadosCorrectos( tablaDeTransiciones, listaDeEstados, lineaNueva )
         return tablaDeTransiciones
 
 
+    # En el siguiente método verificamos que el número de entradas sean correctas
     def comprobarFormatoDelArchivo( self, lista, lineaNueva ):
-        if ( len( lista[0] ) != ( len( lista[1] ) - 2 ) ):
+        if ( len( lista[0] ) != ( len( lista[1] ) - 2 ) ):     
             print( "El número de entradas no conincide con las transiciones" )
             print( "verifique el archivo en la fila: 1 y por favor corregir" )
             exit()
+        self.comprobarNumeroDeTransiciones( lista, lineaNueva )
+        self.comprobarEstadosDeAceptacion( lista, lineaNueva )
+
+
+    # En el siguiente método comprobamos que se tenga el mismo número de columnas
+    # sin contar la primera fila que son las entradas
+    def comprobarNumeroDeTransiciones(self, lista, lineaNueva):
         for val in range ( 2, len( lista ) ):
             anterior = val - 1
             if ( len( lista[val] ) != len( lista[anterior] ) ):
                 if( lineaNueva ):
-                    print( "El estado ingresado tiene más o menos transiciones que los demás" )
-                    self.eliminarLineaNueva()
+                    if( self.rompeCiclos ):
+                        break
+                    else:
+                        print( "\nLa linea ingresada no es válida" )
+                        self.eliminarLineaNueva()
+                        break
                 else:
                     print( "Existe un estado con más o menos transiciones que los demás" )
                     print( "Verifique el archivo en la fila: ", (val+1), " y por favor corregir" )
                     exit()
+            
+
+    # El siguiente método comprueba que la última columna (Columna de estados de aceptación) contenga sólo 0 ó 1
+    def comprobarEstadosDeAceptacion( self, lista, lineaNueva ):
         for val in range( 1, len( lista ) ):
             if ( (lista[val][-1] != '0') and (lista[val][-1] != '1') ):
                 if( lineaNueva ):
-                    print( "El estado de aceptación es incorrecto" )
-                    self.eliminarLineaNueva()
+                    if( self.rompeCiclos ):
+                        break
+                    else:
+                        print( "\nLa linea ingresada no es válida" )
+                        self.eliminarLineaNueva()
+                        break
                 else:
                     print( "Algún estado de aceptación es incorrecto" )
                     print( "Verifique el archivo en la fila: ", (val+1), " y por favor corregir" )
                     exit()
-        
 
+
+    # En este método guardamos los estados en una lista
     def buscarListaDeEstados( self, lista ):
         listaEstados = []
         for val in range( 1, len( lista ) ):
@@ -53,26 +78,36 @@ class ManejoDeArchivo:
         return listaEstados
 
 
+    # En este método verificamos que las transiciones se hagan a estados correctos.
+    # Miramos la matriz sin la primera fila (fila de entradas), sin la última columna (estados de acpetación)
+    # Y sin la primera columna que son los estados; verificamos entonces que esa matriz contenga valores 
+    # existentes en la lista que guarda los estados
     def comprobarTransicionesAEstadosCorrectos( self, lista, listaEstados, lineaNueva ):
         for x in range( 1, len(lista) ):
             for y in range( 1, (len(lista[x]) - 1) ):
                 if( listaEstados.count( lista[x][y] ) == 0 ):
                     if( lineaNueva ):
-                        print( "Alguna de las transiciones de la linea nueva está incorrecta" )
-                        self.eliminarLineaNueva()
+                        if( self.rompeCiclos ):
+                            break
+                        else:
+                            print( "\nLa linea ingresada no es válida" )
+                            self.eliminarLineaNueva()
+                            break
                     else:
                         print( "Alguna de las transiciones está incorrecta" )
                         print( "Verifique el archivo en la fila: ", (x+1), " y por favor corregir" )
                         exit()
 
     
+    # Este método es llamado sólo en caso de que se agregue una nueva fila y la fila esté errónea
     def eliminarLineaNueva( self ):
-        print( "\nLa linea ingresada no es válida" )
-        #file1 = open( self.nombreArchivo, "r" )
-        #lineasArchivo = file1.readlines()
-        #lineasArchivo = lineasArchivo[:-1]
-        #file1.close()
-        #file2 = open( self.nombreArchivo, "w" )
-        #file2.writelines( lineasArchivo )
-        #file2.close()
+        self.rompeCiclos = True
+        file1 = open( self.nombreArchivo, "r" )
+        lineasArchivo = file1.readlines()
+        lineasArchivo = lineasArchivo[:-1]      # Se elimina la última fila ingresada
+        lineasArchivo[-1] = lineasArchivo[-1].strip()       # Borramos el salto de línea
+        file1.close()
+        file2 = open( self.nombreArchivo, "w" )
+        file2.writelines( lineasArchivo )   # Reescribimos el archivo de nuevo, esta vez sin la línea errónea
+        file2.close()
         
